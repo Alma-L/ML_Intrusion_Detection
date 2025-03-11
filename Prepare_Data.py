c3cdac7bcfdfc375446c8988912a2663a36a8385
RinesaBislimi <rinesa.bislimi@student.uni-pr.edu>
2025-03-11 20:42:49 +0100

    Analyze skewness of numerical features

--- Prepare_Data.py
+++ Prepare_Data.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from tabulate import tabulate
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv('Data/intrusion_traffic_data.csv')
 
@@ -26,3 +29,10 @@ outlier_mask = (numeric_cols < (Q1 - 1.5 * IQR)) | (numeric_cols > (Q3 + 1.5 * I
 outlier_counts = outlier_mask.sum()
 print("\n### Number of Outliers per Column ###")
 print(outlier_counts)
+
+
+# Analyze skewness of numerical features
+numerical_features = ['Duration', 'PacketCount', 'ByteCount', 'network_packet_size', 'session_duration', 'ip_reputation_score', 'failed_logins']
+print("\nSkewness of numerical features:")
+skewness_values = {feature: df[feature].skew() for feature in numerical_features}
+print(tabulate(skewness_values.items(), headers=['Feature', 'Skewness'], tablefmt='pretty', floatfmt=".4f"))
